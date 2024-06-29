0afcea7df8c5e75214a1ac714bbc83034902822e
GuilhermeMarques1 <g-marques@outlook.com>
2024-06-29 10:20:02 -0300

    feat: Add route to create user

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (req, reply) => {
+  const registerUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-prisma.user.create({
-  data: {
-    name: 'Guilherme',
-    email: 'gui@dev.com',
-  },
+  const { name, email, password } = registerUserSchema.parse(req.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
 })
