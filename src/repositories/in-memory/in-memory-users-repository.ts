ce58b3356836ebfb15c664f805bcef42bc524343
GuilhermeMarques1 <g-marques@outlook.com>
2024-07-24 12:24:27 -0300

    feat: Add method findById to UsersRepository

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,8 +1,18 @@
-import { UsersRepository } from '@/repositories/prisma/users-repository'
+import { UsersRepository } from '../users-repository'
 import { Prisma, User } from '@prisma/client'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
+
+  async findById(userId: string) {
+    const user = this.items.find((user) => user.id === userId)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
 
   async findByEmail(email: string) {
     const user = this.items.find((user) => user.email === email)
