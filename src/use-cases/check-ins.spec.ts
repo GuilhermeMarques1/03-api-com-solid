1736f89f797b5c8b63f3aa19c9380f9d6916728f
GuilhermeMarques1 <g-marques@outlook.com>
2024-07-29 15:33:37 -0300

    feat: Add TDD to check in the same day and mock data

--- src/use-cases/check-ins.spec.ts
+++ src/use-cases/check-ins.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
 
@@ -9,6 +9,12 @@ describe('Check In Use Case', async () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
@@ -18,5 +24,21 @@ describe('Check In Use Case', async () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2024, 0, 1, 8, 0))
+
+    await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        userId: 'user-01',
+        gymId: 'gym-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
