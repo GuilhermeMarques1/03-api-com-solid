157b7f70edff658424d8c477b5f5229299c70e4d
GuilhermeMarques1 <g-marques@outlook.com>
2024-07-30 00:56:16 -0300

    feat: Add use case to fetch check ins history

---
+++ src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,35 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { FetchUserCheckInsHistoryUseCase } from './fetch-user-check-ins-history'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: FetchUserCheckInsHistoryUseCase
+
+describe('Fetch Check-in History use case', async () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new FetchUserCheckInsHistoryUseCase(checkInsRepository)
+  })
+
+  it('should be able to fetch check in history', async () => {
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-01',
+    })
+
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-02',
+    })
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-01' }),
+      expect.objectContaining({ gym_id: 'gym-02' }),
+    ])
+  })
+})
